0e7c05bca2984c71d67bd74bdd2e4a5dc5f60d34
maickol12 <rocm960107@outlook.com>
2017-07-21 19:44:56 -0500

    cajas

---
+++ src/esqueletos/caja.java
@@ -0,0 +1,71 @@
+package esqueletos;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+public class caja {
+	private int idcaja,is_open,is_active;
+	private String codigo_caja;
+	
+	@Column(name = "f_alta",columnDefinition="DATETIME")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date f_alta;
+
+	@Column(name = "f_baja",columnDefinition="DATETIME")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date f_baja;
+
+	public int getIdcaja() {
+		return idcaja;
+	}
+
+	public void setIdcaja(int id_caja) {
+		this.idcaja = id_caja;
+	}
+
+	public int getIs_open() {
+		return is_open;
+	}
+
+	public void setIs_open(int is_open) {
+		this.is_open = is_open;
+	}
+
+	public int getIs_active() {
+		return is_active;
+	}
+
+	public void setIs_active(int is_active) {
+		this.is_active = is_active;
+	}
+
+	public String getCodigo_caja() {
+		return codigo_caja;
+	}
+
+	public void setCodigo_caja(String codigo_caja) {
+		this.codigo_caja = codigo_caja;
+	}
+
+	public Date getF_alta() {
+		return f_alta;
+	}
+
+	public void setF_alta(Date f_alta) {
+		this.f_alta = f_alta;
+	}
+
+	public Date getF_baja() {
+		return f_baja;
+	}
+
+	public void setF_baja(Date f_baja) {
+		this.f_baja = f_baja;
+	}
+	
+	
+	
+}
